2f7baed955eeacaa71b44e2cae940af452b6abd5
Andri Sveinbjörnsson <Andri Sveinbjrnsson>
2016-12-05 16:38:19 +0000

    UI added computers

--- Skil2/consoleui.cpp
+++ Skil2/consoleui.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include "consoleui.h"
 #include "person.h"
+#include "computer.h"
 #include "dataaccess.h"
 
 using namespace std;
@@ -78,14 +79,59 @@ void ConsoleUI::run()
             }
             break;
         case 3:
-            search();
-            break;
+            cout << "Do you want to delete out a list of person or a computer?\n";
+            cout << "\t1. Print out person  \n";
+            cout << "\t2. Print out computer \n";
+            cout << "Your choice: ";
+            int searchChoice;
+            cin >> searchChoice;
+            switch(searchChoice)
+            {
+            case 1:
+                searchPerson();
+                break;
+            case 2:
+                searchComputer();
+                break;
+            default:
+                 cout<<"\tInvalid entry!"<<endl;
+            }
         case 4:
-            deleteFromFile();
-            break;
+            cout << "Do you want to delete out a list of person or a computer?\n";
+            cout << "\t1. Print out person  \n";
+            cout << "\t2. Print out computer \n";
+            cout << "Your choice: ";
+            int deleteFromFileChoice;
+            cin >> deleteFromFileChoice;
+            switch(deleteFromFileChoice)
+            {
+            case 1:
+                deleteFromFilePerson();
+                break;
+            case 2:
+                deleteFromFileComputer();
+                break;
+            default:
+                 cout<<"\tInvalid entry!"<<endl;
+            }
         case 5:
-            edit();
-            break;
+                cout << "Do you want to print out a list of person or a computer?\n";
+                cout << "\t1. Print out person  \n";
+                cout << "\t2. Print out computer \n";
+                cout << "Your choice: ";
+                int editChoice;
+                cin >> editChoice;
+                switch(editChoice)
+                {
+                case 1:
+                    editPerson();
+                    break;
+                case 2:
+                    editComputer();
+                    break;
+                default:
+                     cout<<"\tInvalid entry!"<<endl;
+                }
         case 6:
             exit(0);
             break;
@@ -367,7 +413,7 @@ void ConsoleUI::sortItPerson()
     displayVector(Persons);
 }
 
-void ConsoleUI::search()
+void ConsoleUI::searchPerson()
 {
     PersonService PA;
     vector<Person> SearchPersons;
@@ -496,7 +542,158 @@ void ConsoleUI::search()
     }
 }
 
-void ConsoleUI::deleteFromFile()
+//Laga ad computer
+void ConsoleUI::searchComputer()
+{
+    PersonService PA;
+    vector<Person> SearchPersons;
+
+    char keepSearching = 'y';
+
+    while (keepSearching == 'y')
+    {
+        SearchPersons= _service.serviceToVector(SearchPersons);
+
+        char moreConditions = 'y';
+
+        bool conditionAvailable[] = {0,0,0,0,0}; // Stores types of search you are using.
+        string searchString[5]; // Stores types of search
+        searchString[0] = "\t 1. Name \n";
+        searchString[1] = "\t 2. Gender \n";
+        searchString[2] = "\t 3. Year of birth \n";
+        searchString[3] = "\t 4. Year of death  \n";
+        searchString[4] = "\t 5. Still alive\n";
+
+        while (moreConditions == 'y' && (!(conditionAvailable[0] && conditionAvailable[1] && conditionAvailable[2] && conditionAvailable[3]&& conditionAvailable[4])))
+        {
+            int conditionNum;
+            for (int i = 0; i < 4; i++)
+            {
+                if (!conditionAvailable[i])
+                {
+                    cout << searchString[i];
+                }
+            }
+            cout << "What do you want to search for? ";
+            while(!(cin>>conditionNum))
+            {
+                cin.clear();
+                cin.ignore(10000,'\n');
+                cout << "Enter corresponding number from 1-5: ";
+            }
+            //search by name
+            if ((conditionNum == 1) & (conditionAvailable[0] == 0))
+            {
+                string nameForSearch;
+                cout << "What name do you want to search for? ";
+
+                SearchPersons = PA.SearchName(SearchPersons, nameForSearch);
+            }
+            //search by gender
+            else if ((conditionNum == 2) & (conditionAvailable[1] == 0) )
+            {
+                char genderForSearch;
+                cout << "Which gender do you want to search for (m/f)? ";
+                while(!(cin>>genderForSearch) | !((genderForSearch == 'm')|(genderForSearch == 'M') | (genderForSearch == 'f') | (genderForSearch == 'F')))
+                {
+                    cin.clear();
+                    cin.ignore(10000,'\n');
+                    cout << "Enter only m/M for male or f/F for femaler: ";
+                }
+                SearchPersons = PA.SearchGender(SearchPersons, genderForSearch);
+            }
+            //search by birth year
+            else if ((conditionNum == 3) & (conditionAvailable[2] == 0))
+            {
+                int birthYearForSearch;
+                cout << "What birth year do you want to search for? ";
+                while(!(cin>>birthYearForSearch))
+                {
+                    cin.clear();
+                    cin.ignore(10000,'\n');
+                    cout << "Enter birth year only in numbers(YYYY): ";
+                }
+                SearchPersons = PA.SearchBirthYear(SearchPersons, birthYearForSearch);
+            }
+            //search by death year
+            else if ((conditionNum == 4) & (conditionAvailable[3] == 0))
+            {
+                int deathYearForSearch;
+                cout << "What death year do you want to search for? ";
+                while(!(cin>>deathYearForSearch) )
+                {
+                    cin.clear();
+                    cin.ignore(10000,'\n');
+                    cout << "Enter death year only in numbers(YYYY):  ";
+                }
+                SearchPersons = PA.SearchDeathYear(SearchPersons, deathYearForSearch);
+            }
+            //search by alive
+            else if ((conditionNum == 5) & (conditionAvailable[3] == 0))
+            {
+                SearchPersons = PA.SearchDeathYear(SearchPersons, 0);
+            }
+
+            conditionAvailable[conditionNum-1] = 1;
+
+            //add type of search
+            cout << "Do you want to add another condition (y/n)? ";
+            while(!(cin>>moreConditions) | (!((moreConditions== 'y')|(moreConditions == 'Y') | (moreConditions == 'n') | (moreConditions == 'N'))))
+            {
+                cin.clear();
+                cin.ignore(10000,'\n');
+                cout << "Enter only y/Y for  or n/N: ";
+            }
+            cout << " \n";
+        }
+
+        PA.deleteDublicateVector(SearchPersons);
+
+
+        if (SearchPersons.size() == 0)
+        {
+            cout << "No person matches this search \n";
+        }
+        else
+        {
+            cout << "Displaying search results: \n";
+            displayVector(SearchPersons);
+        }
+
+
+        cout << "\nDo you want to search again(y/n)? ";
+        while(!(cin>>keepSearching) | !((keepSearching== 'y')|(keepSearching == 'Y') | (keepSearching == 'n') | (keepSearching == 'N')))
+        {
+            cin.clear();
+            cin.ignore(10000,'\n');
+            cout << "Enter only y/Y for  or n/N: ";
+        }
+
+    }
+}
+
+void ConsoleUI::deleteFromFileComputer()
+{
+    vector<Computer> Computers;
+    //Computers = _service.serviceToVector(Computers );
+    int id;
+    //displayVector(Computers , 1);
+
+    cout << "\nEnter the ID of the person you want to delete: ";
+    while(!(cin >> id))
+    {
+        cin.clear();
+        cin.ignore(10000,'\n');
+        cout << "Enter only corresponding numbers: ";
+    }
+
+    Computers .erase (Computers .begin()+id-1);
+
+    //_service.serviceToFile(Computers );
+    //displayVector(Computers);
+}
+
+void ConsoleUI::deleteFromFilePerson()
 {
     vector<Person> Persons;
     Persons = _service.serviceToVector(Persons);
@@ -517,7 +714,7 @@ void ConsoleUI::deleteFromFile()
     displayVector(Persons);
 }
 
-void ConsoleUI::edit()
+void ConsoleUI::editPerson()
 {
     string newName;
     int newYear;
@@ -593,6 +790,68 @@ void ConsoleUI::edit()
     }
     _service.serviceToFile(Persons);
     displayVector(Persons);
+}
+
+void ConsoleUI::editComputer()
+{
+    string newName;
+    string newType;
+    int newYear;
+    vector<Computer> Computers;
+    //computer
+   // Computers = _service.serviceToVector(Computers);
+    int id;
+    //couts vector with ID
+    //displayVector( Computers, 1 );
+
+    cout << "\nEnter the ID of the computer you want to edit? ";
+    while(!(cin >> id))
+    {
+        cin.clear();
+        cin.ignore(10000,'\n');
+        cout << "Enter only corresponding numbers: ";
+    }
+    cout << "What do you want to edit?\n "
+         <<"\t 1. Name \n"
+        <<"\t 2. Type \n"
+       <<"\t 3. Build year \n";
+
+    //what to change
+    cout << "Your choice: ";
+    int editChoice;
+    cin >> editChoice;
+    switch(editChoice)
+    {
+    case 1:
+        cout << "Enter new name: ";
+        cin.ignore(10000,'\n');
+        getline(cin, newName);
+        //Persons.at(id-1).setName(newName);
+        break;
+    case 2:
+        cout << "Enter new type: ";
+        cin.ignore(10000,'\n');
+        getline(cin, newType);
+        Computers.at(id-1).setType(newType);
+        break;
+    case 3:
+        cout << "Enter new year: ";
+        while(!(cin>>newYear))
+        {
+            cin.clear();
+            cin.ignore(10000,'\n');
+            cout << "Enter year only in numbers(YYYY): ";
+        }
+        //Persons.at(id-1).setBuildYear(newYear);
+        break;
+    default:
+    {
+        cout<<"\tInvalid entry!"<<endl;
+    }
+
+    }
+   // _service.serviceToFile(Persons);
+   // displayVector(Persons);
 }
 
 void ConsoleUI::displayVector( vector<Person> printIt, int x )
--- Skil2/consoleui.h
+++ Skil2/consoleui.h
@@ -14,9 +14,12 @@ public:
     void writeComputer();
     void sortItPerson();
     void sortItComputer();
-    void search();
-    void deleteFromFile();
-    void edit();
+    void searchPerson();
+    void searchComputer();
+    void deleteFromFilePerson();
+    void deleteFromFileComputer();
+    void editPerson();
+    void editComputer();
     void displayVector(vector<Person> printIt, int x = 0);
     void teamLogo();
 private:
