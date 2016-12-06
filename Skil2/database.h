42d61ace41a8add1bec20b81d324090680172aaf
Isabella Finnsdottir <isabellayr@Isabellas-MacBook-Pro.local>
2016-12-06 13:22:44 +0000

    Made a database for sqlite

---
+++ Skil2/database.h
@@ -0,0 +1,20 @@
+#ifndef DATABASE_H
+#define DATABASE_H 
+#include <QtSql>
+#include "person.h"
+#include <vector>
+using namespace std;
+
+
+class Database
+{
+public:
+    Database();
+    void quit();
+
+private:
+    QSqlDatabase _db;
+};
+
+
+#endif // DATABASE_H
